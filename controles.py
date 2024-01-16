f453e845a24189b938c512f3fb3b6b0c7086a492
Lucas Curty <lukas.curty.21@gmail.com>
2024-01-16 17:36:35 -0300

    Arrumando os nao encontrados

--- controles.py
+++ controles.py
@@ -1,9 +1,5 @@
 import os
-import time
-import pyautogui
-from acessos import CAMINHO, ANT_CAMINHO
-from main import driver
-
+from acessos import CAMINHO
 
 def canhoto_pendente(canhoto,valor):
     if valor == canhoto:
@@ -13,8 +9,6 @@ def canhoto_pendente(canhoto,valor):
         arquivo_destino = os.path.join(f'{CAMINHO}/CANHOTOS_PENDENTES/',canhoto)
         os.rename(arquivo_origem, arquivo_destino)
         print(f"-----------{canhoto}------------- MOVIDO!")
-        time.sleep(1)
-        se_pendente(canhoto)
 
 def canhoto_digitalizado(canhoto,valor):
     if valor == canhoto:
@@ -34,16 +28,12 @@ def canhoto_rejeitado(canhoto,valor):
         arquivo_destino = os.path.join(f'{CAMINHO}/CANHOTOS_REJEITADOS/',canhoto)
         os.rename(arquivo_origem, arquivo_destino)
         print(f"-----------{canhoto}------------- MOVIDO!")
-        time.sleep(1)
-        se_pendente(canhoto)
-    
-    
-def se_pendente(canhoto):
-    driver.find_element('xpath','/html/body/div[2]/div/div/main/section/div[2]/div[2]/div/div/div[2]/div[1]/div[1]/div[2]/div/div[2]/div/div/table/tbody/tr/td[13]/div/button').click()
-    driver.find_element('xpath','/html/body/div[2]/div/div/main/section/div[2]/div[2]/div/div/div[2]/div[1]/div[1]/div[2]/div/div[2]/div/div/table/tbody/tr/td[13]/div/div/a[4]').click()
-    time.sleep(3)
-    pyautogui.typewrite(f'{ANT_CAMINHO}\{canhoto}')
-    pyautogui.press('enter')
-    time.sleep(1)
-    pyautogui.press('enter')
-    print(f"------------{canhoto}-- ENVIADO PARA A DANONE ---------------------")
+
+def canhoto_Aguardando_Aprovacao(canhoto,valor):
+    if valor == canhoto:
+        print(f'{valor} encontrado')
+        print(f'Movendo canhoto {valor} para a pasta CANHOTOS_AG_APROVAÇÃO')
+        arquivo_origem = os.path.join(f'{CAMINHO}/CANHOTOS/',canhoto)
+        arquivo_destino = os.path.join(f'{CAMINHO}/CANHOTOS_AG_APROVAÇÃO/',canhoto)
+        os.rename(arquivo_origem, arquivo_destino)
+        print(f"-----------{canhoto}------------- MOVIDO!")
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from controles import * 
+from controles import *
 from selenium import webdriver
-from acessos import LOGIN,SENHA,URL
+from acessos import LOGIN,SENHA,URL,CAMINHO, ANT_CAMINHO
 from pastas import folder_canhotos,folder
 import time
 import pyautogui
+import os
 
 driver = webdriver.Chrome()
 driver.get(URL)
@@ -20,6 +21,17 @@ driver.find_element('xpath','//*[@id="js-nav-menu"]/li[6]/ul/li[1]/ul/li[1]/a').
 time.sleep(2)
 driver.find_element('xpath','/html/body/div[2]/div/div/main/section/div[2]/div[1]/div/div/div/div/div[1]/div[1]/div[4]/div/div/input').clear()
 time.sleep(1.7)
+
+def se_pendente(valor):
+    driver.find_element('xpath','/html/body/div[2]/div/div/main/section/div[2]/div[2]/div/div/div[2]/div[1]/div[1]/div[2]/div/div[2]/div/div/table/tbody/tr/td[13]/div/button').click()
+    driver.find_element('xpath','/html/body/div[2]/div/div/main/section/div[2]/div[2]/div/div/div[2]/div[1]/div[1]/div[2]/div/div[2]/div/div/table/tbody/tr/td[13]/div/div/a[4]').click()
+    time.sleep(3)
+    pyautogui.typewrite(f'{ANT_CAMINHO}\{valor}')
+    pyautogui.press('enter')
+    time.sleep(1)
+    pyautogui.press('enter')
+    print(f"------------{valor}-- ENVIADO PARA A DANONE ---------------------")
+
                   
 def findCanhoto(canhoto):
     driver.find_element('xpath','/html/body/div[2]/div/div/main/section/div[2]/div[1]/div/div/div/div/div[1]/div[1]/div[1]/div/input').clear()
@@ -28,42 +40,62 @@ def findCanhoto(canhoto):
     time.sleep(2)
     pyautogui.press('pagedown')
     time.sleep(1)
-    valor = driver.find_element('xpath','/html/body/div[2]/div/div/main/section/div[2]/div[2]/div/div/div[2]/div[1]/div[1]/div[2]/div/div[2]/div/div/table/tbody/tr/td[1]').text
-    status = driver.find_element('xpath','/html/body/div[2]/div/div/main/section/div[2]/div[2]/div/div/div[2]/div[1]/div[1]/div[2]/div/div[2]/div/div/table/tbody/tr/td[8]').text
-    if valor:
-        if status == "Digitalizado":
-            print(f"O Canhoto {valor} encontrase com o status de: {status}")
-            for canhoto in folder:
-                if canhoto == f'{valor}.png':
-                    canhoto_digitalizado(canhoto,f'{valor}.png')
-                if canhoto == f'{valor}.jpg':
-                    canhoto_digitalizado(canhoto,f'{valor}.jpg')
-                
-        if status == "Pendente":
-            print(f"O Canhoto {valor} encontrase com o status de: {status}")
-            for canhoto in folder:
-                if canhoto == f'{valor}.png':
-                    time.sleep(1)
-                    canhoto_pendente(canhoto,f'{valor}.png')
-                if canhoto == f'{valor}.jpg':
-                    time.sleep(1)
-                    canhoto_pendente(canhoto,f'{valor}.jpg')
+    try:
+        valor = driver.find_element('xpath','/html/body/div[2]/div/div/main/section/div[2]/div[2]/div/div/div[2]/div[1]/div[1]/div[2]/div/div[2]/div/div/table/tbody/tr/td[1]').text
+        status = driver.find_element('xpath','/html/body/div[2]/div/div/main/section/div[2]/div[2]/div/div/div[2]/div[1]/div[1]/div[2]/div/div[2]/div/div/table/tbody/tr/td[8]').text
+        if valor:
+            if status == "Ag. Aprovação":
+                print(f"O Canhoto {valor} encontrase com o status de: {str.upper(status)}")
+                for canhoto in folder:
+                    if canhoto == f'{valor}.png':
+                        canhoto_Aguardando_Aprovacao(canhoto,f'{valor}.png')
+                    if canhoto == f'{valor}.jpg':
+                        canhoto_Aguardando_Aprovacao(canhoto,f'{valor}.jpg')
+                        
+            if status == "Digitalizado":
+                print(f"O Canhoto {valor} encontrase com o status de: {str.upper(status)}")
+                for canhoto in folder:
+                    if canhoto == f'{valor}.png':
+                        canhoto_digitalizado(canhoto,f'{valor}.png')
+                    if canhoto == f'{valor}.jpg':
+                        canhoto_digitalizado(canhoto,f'{valor}.jpg')
+                    
+            if status == "Pendente":
+                print(f"O Canhoto {valor} encontrase com o status de: {str.upper(status)}")
+                for canhoto in folder:
+                    if canhoto == f'{valor}.png':
+                        se_pendente(valor)
+                        time.sleep(1)
+                        canhoto_pendente(canhoto,f'{valor}.png')
+                        
+                    if canhoto == f'{valor}.jpg':
+                        se_pendente(valor)
+                        time.sleep(1)
+                        canhoto_pendente(canhoto,f'{valor}.jpg')
 
-        if status == "Rejeitada":
-            print(f"O Canhoto {valor} encontrase com o status de: {status}")
-            for canhoto in folder:
-                if canhoto == f'{valor}.png':
-                    time.sleep(1)
-                    canhoto_rejeitado(canhoto,f'{valor}.png')
-                if canhoto == f'{valor}.jpg':
-                    time.sleep(1)
-                    canhoto_rejeitado(canhoto,f'{valor}.jpg')
-                
-        time.sleep(3)
-    if not valor:
-        print(f"Canhoto: {valor} NAO ENCONTRADO")
-        pass
-
+            if status == "Rejeitada":
+                print(f"O Canhoto {valor} encontrase com o status de: {str.upper(status)}")
+                for canhoto in folder:
+                    if canhoto == f'{valor}.png':
+                        se_pendente(valor)
+                        time.sleep(1)
+                        canhoto_rejeitado(canhoto,f'{valor}.png')
+                    if canhoto == f'{valor}.jpg':
+                        se_pendente(valor)
+                        time.sleep(1)
+                        canhoto_rejeitado(canhoto,f'{valor}.jpg')
+                    
+            time.sleep(3)
+        if not valor or ValueError:
+            print('NAO ENCONTRADO')
+    except Exception:
+        print(f'{canhoto}------------NAO ENCONTRADO -----------------')
+        arquivo_origem = os.path.join(CAMINHO,'CANHOTOS/',canhoto)
+        arquivo_destino = os.path.join(CAMINHO,'CANHOTOS_NAO_ENCONTRADOS/',canhoto)
+        os.rename(arquivo_origem, arquivo_destino)
+        print(f'{canhoto}------------CANHOTO MOVIDO-----------------')
+       
+            
 for item in folder_canhotos:
     print(f'-------Tentando encontrar o canhoto: {item}-------')
     findCanhoto(item)
